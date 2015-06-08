9aa683dd54d8c4db65338563042a55588dd00e2b
Gordon Klaus <gordon.klaus@gmail.com>
2015-06-08 23:07:49 +0200

    Refine checker: report errors inside loops and function literals.

--- ineffassign.go
+++ ineffassign.go
@@ -60,106 +60,171 @@ func checkPath(path string) (errors int) {
 		return errors
 	}
 
-	chk := &checker{map[*ast.Object]*ast.Ident{}, map[*ast.Object]bool{}, 0, 0}
+	chk := checker{}
+	// Package-level vars are exempt.
 	for _, d := range f.Decls {
 		if d, ok := d.(*ast.GenDecl); ok && d.Tok == token.VAR {
 			for _, s := range d.Specs {
-				for _, i := range s.(*ast.ValueSpec).Names {
-					chk.escapes[i.Obj] = true
+				for _, id := range s.(*ast.ValueSpec).Names {
+					chk.info(id).escapes = true
 				}
 			}
 		}
 	}
 	ast.Walk(chk, f)
-	for _, i := range chk.assignedNotUsed {
-		if !chk.escapes[i.Obj] {
-			fmt.Printf("%s: %s is assigned but not used\n",
-				fset.Position(i.Pos()), i.Name)
+	for _, i := range chk {
+		if !i.escapes && i.assign > i.use {
+			fmt.Printf("%s: %s assigned and not used\n", fset.Position(i.id.Pos()), i.id.Name)
 			errors++
 		}
 	}
 	return errors
 }
 
-type checker struct {
-	assignedNotUsed map[*ast.Object]*ast.Ident
-	escapes         map[*ast.Object]bool
-	loops, funcLits int
+type checker map[*ast.Object]*info
+
+type info struct {
+	id            *ast.Ident
+	assign, use   token.Pos
+	loop, funclit ast.Node
+	escapes       bool
 }
 
-// TODO: Be less conservative: Take variable scope into consideration in loops and func literals.
-func (chk *checker) Visit(n ast.Node) ast.Visitor {
+func (chk checker) Visit(n ast.Node) ast.Visitor {
 	switch n := n.(type) {
+	case *ast.GenDecl:
+		if n.Tok == token.VAR {
+			for _, s := range n.Specs {
+				for _, id := range s.(*ast.ValueSpec).Names {
+					chk.info(id)
+				}
+			}
+		}
 	case *ast.AssignStmt:
-		for _, x := range append(n.Rhs, n.Lhs...) {
+		for _, x := range n.Rhs {
 			ast.Walk(chk, x)
 		}
-		if n.Tok == token.ASSIGN {
-			for _, x := range n.Lhs {
-				if i, ok := unparen(x).(*ast.Ident); ok {
-					// Conservatively ignore assignments inside loops.
-					if chk.loops == 0 && i.Obj != nil {
-						chk.assignedNotUsed[i.Obj] = i
-					}
-				}
+		for _, x := range n.Lhs {
+			if id, ok := unparen(x).(*ast.Ident); ok {
+				chk.info(id).assign = n.End()
+			} else {
+				ast.Walk(chk, x)
 			}
 		}
 		return nil
 	case *ast.BranchStmt:
-		// A goto may act as a loop.  Conservatively ignore any assignments occurring before it.
 		if n.Tok == token.GOTO {
-			chk.assignedNotUsed = map[*ast.Object]*ast.Ident{}
-		}
-	case *ast.FuncType:
-		if n.Results != nil {
-			for _, f := range n.Results.List {
-				for _, i := range f.Names {
-					chk.escapes[i.Obj] = true
+			for _, i := range chk {
+				if i.use > n.Label.Obj.Decl.(*ast.LabeledStmt).Pos() && i.use < n.Pos() {
+					i.use = n.Pos()
 				}
 			}
 		}
+	case *ast.FuncDecl:
+		if n.Recv != nil {
+			for _, id := range n.Recv.List[0].Names {
+				chk.info(id)
+			}
+		}
+	case *ast.FuncType:
+		if n.Params != nil {
+			for _, f := range n.Params.List {
+				for _, id := range f.Names {
+					chk.info(id)
+				}
+			}
+		}
+		if n.Results != nil {
+			for _, f := range n.Results.List {
+				for _, id := range f.Names {
+					chk.info(id).escapes = true
+				}
+			}
+		}
+		return nil
 	case *ast.Ident:
-		delete(chk.assignedNotUsed, n.Obj)
-		// Conservatively mark any variable mentioned in a func literal as escaping.
-		if chk.funcLits > 0 {
-			chk.escapes[n.Obj] = true
+		i := chk.info(n)
+		i.use = n.Pos()
+		if i.funclit != nil {
+			i.escapes = true
+		}
+		if i.loop != nil {
+			i.use = i.loop.End()
 		}
 	case *ast.UnaryExpr:
-		if i, ok := unparen(n.X).(*ast.Ident); n.Op == token.AND && ok {
-			chk.escapes[i.Obj] = true
+		if id, ok := unparen(n.X).(*ast.Ident); ok && n.Op == token.AND {
+			chk.info(id).escapes = true
 		}
-	case *ast.CallExpr:
-		// A method call might implicitly take the address of its receiver, causing it to escape.
+	case *ast.SelectorExpr:
+		// A method call (possibly delayed via a method value) might implicitly take
+		// the address of its receiver, causing it to escape.
 		// We can't do any better here without knowing the variable's type.
-		if s, ok := unparen(n.Fun).(*ast.SelectorExpr); ok {
-			if i, ok := unparen(s.X).(*ast.Ident); ok {
-				chk.escapes[i.Obj] = true
-			}
+		if id, ok := unparen(n.X).(*ast.Ident); ok {
+			chk.info(id).escapes = true
 		}
 	case *ast.ForStmt:
 		walk(chk, n.Init)
-		chk.loops++
+		chk.loop(n, true)
 		walk(chk, n.Cond)
 		walk(chk, n.Post)
 		walk(chk, n.Body)
-		chk.loops--
+		chk.loop(n, false)
 		return nil
 	case *ast.RangeStmt:
 		walk(chk, n.X)
-		chk.loops++
-		walk(chk, n.Key)
-		walk(chk, n.Value)
+		chk.loop(n, true)
+		if n.Key != nil {
+			lhs := []ast.Expr{n.Key}
+			if n.Value != nil {
+				lhs = append(lhs, n.Value)
+			}
+			walk(chk, &ast.AssignStmt{Lhs: lhs, Tok: n.Tok, TokPos: n.TokPos, Rhs: []ast.Expr{&ast.Ident{NamePos: n.X.End()}}})
+		}
 		walk(chk, n.Body)
-		chk.loops--
+		chk.loop(n, false)
 		return nil
 	case *ast.FuncLit:
 		walk(chk, n.Type)
-		chk.funcLits++
+		chk.funclit(n, true)
 		walk(chk, n.Body)
-		chk.funcLits--
+		chk.funclit(n, false)
 		return nil
 	}
 	return chk
+}
+
+func (chk checker) info(id *ast.Ident) *info {
+	i := chk[id.Obj]
+	if i == nil {
+		i = &info{}
+		if id.Obj != nil && id.Name != "_" {
+			chk[id.Obj] = i
+		}
+	}
+	i.id = id
+	return i
+}
+
+func (chk checker) loop(loop ast.Node, enter bool) {
+	for _, i := range chk {
+		if enter && i.loop == nil {
+			i.loop = loop
+		}
+		if !enter && i.loop == loop {
+			i.loop = nil
+		}
+	}
+}
+
+func (chk checker) funclit(funclit ast.Node, enter bool) {
+	for _, i := range chk {
+		if enter && i.funclit == nil {
+			i.funclit = funclit
+		}
+		if !enter && i.funclit == funclit {
+			i.funclit = nil
+		}
+	}
 }
 
 func walk(v ast.Visitor, n ast.Node) {
